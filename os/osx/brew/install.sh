a7a02b221553dfa05c83dc4d19609c5ffa625f48
hiroshiro <agend22@gmail.com>
2016-02-24 13:07:06 +0900

    [ script ] Refactoring. [ os ] Add directory for os. [ brew ] Make install's script.

---
+++ os/osx/brew/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+</dev/null
---
+++ os/ubuntu/brew/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/linuxbrew/go/install)" \
+</dev/null

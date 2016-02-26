73ca8a3497ed2171cd75bb4a78ac6b99c489c392
hiroshiro <agend21@icloud.com>
2016-02-26 16:48:25 +0900

    [ shell ] [ brew ]  Fix.

--- os/osx/brew/install.sh
+++ os/osx/brew/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source '../../../shell/utils.sh'
+source 'shell/utils.sh'
 
 if ! cmd_exists 'brew'; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
--- shell/utils.sh
+++ shell/utils.sh
@@ -4,6 +4,7 @@ cmd_exists() {
     command -v "$1" &> /dev/null
     return $?
 }
+
 get_os() {
 
     declare -r OS_NAME="$(uname -s)"

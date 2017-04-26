ea4716bbb7bd38f195db67a457cd74f18640f873
hiroshiro <agend21@icloud.com>
2017-04-26 17:38:42 +0900

    [os/ubuntu/brew/bundle.sh][os/osx/brew/bundle.sh] Delete update.

--- os/osx/brew/bundle.sh
+++ os/osx/brew/bundle.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-brew update
-
 brew bundle check >/dev/null 2>&1  || {
   echo "==> Installing Homebrew dependencies…"
   brew bundle
--- os/ubuntu/brew/bundle.sh
+++ os/ubuntu/brew/bundle.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-brew update
-
 brew bundle check >/dev/null 2>&1  || {
   echo "==> Installing Linuxbrew dependencies…"
   brew bundle
